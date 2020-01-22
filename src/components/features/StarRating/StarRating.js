9f93359806ffe2dc604a322324f97e512900bb83
Maria Kasińska <mariakasinska17@gmail.com>
2020-01-22 13:02:17 +0100

    add onMouseOver and onMouseOut to StarRating component

--- src/components/features/StarRating/StarRating.js
+++ src/components/features/StarRating/StarRating.js
@@ -8,26 +8,67 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
 
 class StarRating extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rating: props.rating,
+      lastRating: props.rating,
+      isRating: false,
+    };
+  }
+  starOver(rating, e) {
+    this.setState({
+      rating,
+      isRating: true,
+    });
+  }
+  starOut() {
+    let { lastRating } = this.state;
+    this.setState({
+      rating: lastRating,
+      isRating: false,
+    });
+  }
   render() {
-    const { stars } = this.props;
-    return (
-      <div className={styles.stars}>
-        {[1, 2, 3, 4, 5].map(i => (
-          <a key={i} href='#'>
-            {i <= stars ? (
-              <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
-            ) : (
-              <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
-            )}
-          </a>
-        ))}
-      </div>
-    );
+    let stars = [];
+    for (let i = 0; i < 5; i++) {
+      let icon;
+      let classy;
+      if (this.props.stars > i && this.props.stars != null) {
+        icon = faStar;
+        if (this.state.rating > i && this.state.rating != null) {
+          classy = styles.selected;
+        }
+      } else {
+        if (this.state.rating > i && this.state.rating != null) {
+          classy = styles.selected;
+          icon = faStar;
+        } else {
+          icon = farStar;
+        }
+      }
+
+      stars.push(
+        <a key={i} href='#'>
+          <FontAwesomeIcon
+            icon={icon}
+            className={classy}
+            onClick
+            onMouseOver={this.starOver.bind(this, i + 1)}
+            onMouseOut={this.starOut.bind(this)}
+          >
+            {i} stars
+          </FontAwesomeIcon>
+        </a>
+      );
+    }
+    return <div className={styles.stars}>{stars}</div>;
   }
 }
 
 StarRating.propTypes = {
   stars: PropTypes.number,
+  rating: PropTypes.number,
 };
 
 export default StarRating;
