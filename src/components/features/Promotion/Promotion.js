8b3ab45d0cf4c65dc97885e62d071dd1f7b56a74
Ola Siem <info@siemieniuk.net>
2020-01-23 15:23:34 +0100

    Add promo boxes and styling

--- src/components/features/Promotion/Promotion.js
+++ src/components/features/Promotion/Promotion.js
@@ -7,22 +7,22 @@ const Promotion = ({ title, subtitle, discount, image }) => (
   <div className={styles.root}>
     <div className='container'>
       <div className='row'>
-        <div className={'col-6'}>
-          <div className={styles.box_one}>
+        <div className={'col-sm-12 col-md-6'}>
+          <div className={`${styles.box} ${styles.one}`}>
             <p>Guest Room</p>
             <p>Sofa</p>
             <p>-20%</p>
           </div>
         </div>
-        <div className='col-6 '>
-          <div className={styles.box_two}>
+        <div className={'col-sm-12 col-md-6 py-3 py-md-0'}>
+          <div className={`${styles.box} ${styles.two}`}>
             <p>
               <span>Office</span> Chair
             </p>
             <p>Collection</p>
             <p>$200.00</p>
           </div>
-          <div className={styles.box_three}>
+          <div className={`${styles.box} ${styles.three}`}>
             <p>
               <span>Special</span> Collection
             </p>
@@ -33,14 +33,6 @@ const Promotion = ({ title, subtitle, discount, image }) => (
     </div>
   </div>
 );
-
-// class Promotion extends React.Component {
-//   render() {
-//     return (
-//       <img src={sofa} alt={'sofa'}/>
-//     );
-//   }
-// }
 
 Promotion.propTypes = {
   title: PropTypes.string,
