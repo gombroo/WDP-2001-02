74366caf16f6bb7d7c5bdcd60ca47c1a4c43735a
Misiek40k <comanderrr@gmail.com>
2020-02-01 13:00:49 +0100

    Add StarRating component to FeaturedBox

--- src/components/common/FeaturedBox/FeaturedBox.js
+++ src/components/common/FeaturedBox/FeaturedBox.js
@@ -1,20 +1,21 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faStar,
   faExchangeAlt,
   faShoppingBasket,
   faEye,
 } from '@fortawesome/free-solid-svg-icons';
-import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 
 import Button from '../Button/Button';
+import StarRating from '../../features/StarRating/StarRating';
 
 import styles from './FeaturedBox.module.scss';
 
-const FeaturedBox = ({ name, price, isUnmounted }) => {
+
+const FeaturedBox = ({ name, price, photo, isUnmounted }) => {
   useEffect(() => {
     document.querySelectorAll('#fadeFeatured').forEach(item => {
       !isUnmounted
@@ -27,50 +28,46 @@ const FeaturedBox = ({ name, price, isUnmounted }) => {
     <div id='fadeFeatured' className={styles.root}>
       <div className={styles.photo}>
         <img
-          src='https://images.pexels.com/photos/3316925/pexels-photo-3316925.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+          src={photo}
           alt=''
         />
         <div className={styles.buttons__hover}>
           <Button variant='small'>
             <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
           </Button>
+
           <div className={styles.countdown}>
             <div>
-              25
+              <span>25</span>
               <br />
               <span>DAYS</span>
             </div>
             <div>
-              12
+              <span>12</span>
               <br />
               <span>HRS</span>
             </div>
             <div>
-              34
+              <span>34</span>
               <br />
               <span>MIN</span>
             </div>
             <div>
-              23
+              <span>23</span>
               <br />
               <span>SEC</span>
             </div>
           </div>
         </div>
       </div>
+
       <div className={styles.content}>
         <h5>{name}</h5>
-        <div className={styles.stars}>
-          <a href='#'>
-            <FontAwesomeIcon icon={faStar}> stars</FontAwesomeIcon>
-            <FontAwesomeIcon icon={faStar}> stars</FontAwesomeIcon>
-            <FontAwesomeIcon icon={faStar}> stars</FontAwesomeIcon>
-            <FontAwesomeIcon icon={farStar}> stars</FontAwesomeIcon>
-            <FontAwesomeIcon icon={farStar}> stars</FontAwesomeIcon>
-          </a>
-        </div>
+        <StarRating />
       </div>
+
       <div className={styles.line}></div>
+
       <div className={styles.actions}>
         <div className={styles.outlines}>
           <Button variant='outline'>
@@ -83,9 +80,9 @@ const FeaturedBox = ({ name, price, isUnmounted }) => {
             <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
           </Button>
         </div>
+
         <div className={styles.price}>
           <div className={styles.promoPrice}>$ {price + 30}</div>
-
           <Button noHover variant='small'>
             $ {price}
           </Button>
@@ -97,7 +94,8 @@ const FeaturedBox = ({ name, price, isUnmounted }) => {
 
 FeaturedBox.propTypes = {
   name: PropTypes.string,
-  price: PropTypes.string,
+  price: PropTypes.number,
+  photo: PropTypes.string,
   isUnmounted: PropTypes.bool,
 };
 
